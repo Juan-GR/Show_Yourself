28e8d8d4f3d1afbf0615869d270171fca79ffea0
juan <juangr@alumnos.iesgalileo.es>
2021-02-15 15:47:57 +0100

    Se han añadido los directorios

---
+++ view/principal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Show UrSelf</title>
+    </head>
+    <body>
+        <div>
+            <div>
+                <p>SHOW URSELF</p>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><form action="" method="post"><button type="submit" name="iniciarSesion">Iniciar Sesion</button></form></li>
+                        <li><form action="" method="post"><button type="submit" name="Registrarse">Registrarse</button></form></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </body>
+</html>
