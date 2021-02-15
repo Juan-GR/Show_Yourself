cfade6fe8409fd7b6a3a3d4e0e986b069969a4cc
juan <juangr@alumnos.iesgalileo.es>
2021-02-15 18:01:00 +0100

    Index mejorado

--- view/principal.php
+++ view/principal.php
@@ -7,14 +7,15 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="./styles/style.css">
         <title>Show UrSelf</title>
     </head>
     <body>
-        <div>
-            <div>
-                <p>SHOW URSELF</p>
+        <header class="contenedorHeader">
+            <div class="titulo">
+                <img src="styles/logo.png" alt="logo"/>
             </div>
-            <div>
+            <div class="contenedorBotones">
                 <nav>
                     <ul>
                         <li><form action="" method="post"><button type="submit" name="iniciarSesion">Iniciar Sesion</button></form></li>
@@ -22,6 +23,12 @@ and open the template in the editor.
                     </ul>
                 </nav>
             </div>
-        </div>
+        </header>
+        <main>
+            <section class="seccion1"><img src="styles/img1.jpg"><p>¡Únete, disfruta y haz muchos amigos!</p></section>
+            <section class="seccion2"></section>
+            <section class="seccion3"></section>
+        </main>
+        <footer></footer>
     </body>
 </html>
