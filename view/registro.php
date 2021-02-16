23b1572e23402fdefa776c62bacb69a266724bb6
Juan-GR <>
2021-02-16 22:39:52 +0100

    Se ha introducido el registro

---
+++ view/registro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./styles/estilos.css">
+        <title>Registro Show UrSelf</title>
+    </head>
+    <body>
+        <div class="divformulario">
+        <form method="POST" action="">
+            <div class="div-form">
+                <div class="div-form1">
+                    <p>!Bienvenid@¡</p>
+                    <p>Regístrate para empezar a disfrutar</p>
+                    <label for="usuario">Nombre de usuario</label>
+                    <input type="text" name="usuario" id="usuario" />
+                    <label for="correo">Correo electrónico</label>
+                    <input type="text" name="correo" id="correo" />
+                    <label for="password1">Contraseña</label>
+                    <input type="password" name="password1" id="correo" />
+                    <label for="password2">Repita la contraseña</label>
+                    <input type="password" name="password2" id="correo" />
+                    <button name="registro" value="registro">Registrarse</button>
+                </div>
+                <div class="div-form2"><img src="styles/logo.png" alt="logo"/></div>
+            </div>
+        </form>
+            </div>
+        <?php
+        
+        ?>
+    </body>
+</html>
